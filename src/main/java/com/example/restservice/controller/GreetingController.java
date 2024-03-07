44e8b42bdb261f62b6a382a9a71086d4dcff19c9
Arnab Ray <ray.arnab008@gmail.com>
2024-03-07 22:46:07 +0530

    added versioning

--- src/main/java/com/example/restservice/controller/GreetingController.java
+++ src/main/java/com/example/restservice/controller/GreetingController.java
@@ -4,12 +4,14 @@ import com.example.restservice.model.Greeting;
 import com.example.restservice.service.GreetingService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping("/greet")
 public class GreetingController {
 
     private final GreetingService greetingService;
@@ -19,7 +21,7 @@ public class GreetingController {
         this.greetingService = greetingService;
     }
 
-    @GetMapping("/greeting")
+    @GetMapping("v1/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), greetingService.greet(name));
     }
